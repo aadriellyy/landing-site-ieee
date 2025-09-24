d2a46a3e5d0609f09f47bf304a835b3677b5ff7c
aadriellyy <limaadrielly214@gmail.com>
2025-09-23 22:15:25 -0300

    :sparkles: feat: add voluntarias

--- event-landing/src/sections/Members.jsx
+++ event-landing/src/sections/Members.jsx
@@ -18,6 +18,20 @@ import Natalia from "../assets/Natalia.jpg";
 import Samara from "../assets/samara.JPG";
 import Daniely from "../assets/daniely.png";
 import Julia from "../assets/julia.png";
+import Camila from "../assets/Camila_Sousa.jpg";
+import Emily from "../assets/emily.jpg";
+import Erica from "../assets/Erica_Feitosa.jpg";
+import EricaTawany from "../assets/Erica_Tawany.jpg";
+import Fernanda from "../assets/Fernanda_Marques.jpeg";
+import Giovanna from "../assets/GiovannaFelipe.jpg";
+import Gisele from "../assets/Gisele_Cristine.jpg";
+import Isabela_Souza from "../assets/Isabela_Souza.jpg";
+import Marcia from "../assets/Marcia_Vieira.jpg";
+import Marjorie from "../assets/MarjorieMitzi.jpg";
+import Mavi from "../assets/mavi.jpg";
+import Michele from "../assets/Michele_Mukaihata.jpeg";
+import Olivia from "../assets/Olivia_Martins.jpg";
+import AnaBeatriz from "../assets/ana-beatriz.png";
 
 export default function ProgramsSection() {
 
@@ -186,6 +200,104 @@ export default function ProgramsSection() {
         "Estudante de Tecnologia em Sistemas para Internet no Instituto Federal de Brasília (IFB).",
       image: Julia,
       link: "https://www.linkedin.com/in/j%C3%BAlia-peghini?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
+    },
+    {
+      title: "Camila Sousa",
+      description:
+        "Estudante de Engenharia de Redes na Universidade de Brasília (UnB).",
+      image: Camila,
+      link: "https://www.linkedin.com/in/camila-sousa-864b4a335/",
+    },
+    {
+      title: "Marjorie Mitzi",
+      description:
+        "Estudante de Engenharia de Software na Universidade de Brasília.",
+      image: Marjorie,
+      link: "https://www.linkedin.com/in/marjorie-mitzi/",
+    },
+    {
+      title: "Emily Bernardes",
+      description:
+        "Estudante de Engenharia de Redes na Universidade de Brasília (UnB).",
+      image: Emily,
+      link: "https://www.linkedin.com/in/emilly-bernardes-351374236",
+    },
+    {
+      title: "Érica Feitosa",
+      description:
+        "Estudante de Ciência da Computação na Universidade de Brasília (UnB).",
+      image: Erica,
+      link: "https://www.linkedin.com/in/ericamfeitosa",
+    },
+    {
+      title: "Erica Tawany",
+      description:
+        "Estudante de Ciência da Computação na Universidade de Brasília (UnB).",
+      image: EricaTawany,
+      link: "https://www.linkedin.com/in/erica-tawany-neres-357573291?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    },
+    {
+      title: "Fernanda Marques",
+      description:
+        "Estudante na Universidade de Brasília (UnB).",
+      image: Fernanda,
+      link: "https://www.linkedin.com/in/fernanda-marques-pereira001/",
+    },
+    {
+      title: "Giovanna Felipe",
+      description:
+        "Estudante de Engenharia de Software na Universidade de Brasília (UnB).",
+      image: Giovanna,
+      link: "https://www.linkedin.com/in/giovanna-felipe-31m2006?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
+    },
+    {
+      title: "Gisele Cristine",
+      description:
+        "Estudante de Engenharia de Redes na Universidade de Brasília (UnB).",
+      image: Gisele,
+      link: "https://www.linkedin.com/in/gisele-cristine-ribeiro-de-carvalho-dev/",
+    },
+    {
+      title: "Isabela Souza",
+      description:
+        "Estudante de Ciência da Computação na Universidade de Brasília (UnB).",
+      image: Isabela_Souza,
+      link: "https://www.linkedin.com/in/isasouzaas/",
+    },
+    {
+      title: "Márcia Vieira",
+      description:
+        "Estudante de Engenharia Elétrica na Universidade de Brasília (UnB).",
+      image: Marcia,
+      link: "https://www.linkedin.com/in/m%C3%A1rcia-nunes-vieira-265008224?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    },
+    {
+      title: "Maria Vitória",
+      description:
+        "Estudante de Engenharia Mecatrônica na Universidade de Brasília (UnB).",
+      image: Mavi,
+      link: "https://www.linkedin.com/in/maria-vit%C3%B3ria-matos-mour%C3%A3o-62187229a/",
+    },
+    {
+      title: "Michele Mukaihata",
+      description:
+        "Estudante Ciência da Computação na Universidade de Brasília (UnB).",
+      image: Michele,
+      link: "www.linkedin.com/in/michele-mukaihata-575b35385",
+    },
+    {
+      title: "Olívia Martins",
+      description:
+        "Estudante de Engenharia de Redes na Universidade de Brasília (UnB).",
+      image: Olivia,
+      link: "https://www.linkedin.com/in/ol%C3%ADvia-martins-07a65834a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    },
+    {
+      title: "Ana Beatriz Goulart",
+      description:
+        "Estudante de Engenharia de Redes na Universidade de Brasília (UnB).",
+      image: AnaBeatriz,
+      link: "https://www.linkedin.com/in/ana-beatriz-goulart-2b057a365/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
     },
 
   ];
